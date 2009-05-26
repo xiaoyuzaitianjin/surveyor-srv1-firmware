ecfbecba0be7248edec72c71c530037dcc13d007
surveyor.com <surveyor.com@ea24bf1e-cabe-11dd-a2b5-e5f827957e07>
2009-05-26 20:45:17 +0000

    added GPL header

--- trunk/blackfin/srv/gps.c
+++ trunk/blackfin/srv/gps.c
@@ -1,3 +1,18 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *  gps.c - navigation functions for the SRV-1 / SVS Blackfin controllers
+ *    Copyright (C) 2005-2009  Surveyor Corporation
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details (www.gnu.org/licenses)
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
 #include "gps.h"
 #include "print.h"
 #include "malloc.h"
